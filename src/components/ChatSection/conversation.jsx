53c79261fdf24a234d2ae4c8cceadcfe8920fd89
Hamza Younsi <younsihamza006@gmail.com>
2024-03-10 20:12:47 +0000

    end

--- src/components/ChatSection/conversation.jsx
+++ src/components/ChatSection/conversation.jsx
@@ -7,12 +7,10 @@ export default function Conversation({ selecteduser }) {
           <pre>{item}</pre>
         </div>
       )).reverse()} */}
-      <div>
         <div id="receved-message">
           <pre>hamza younsi hamza dsad</pre>
           <img src="./src/assets/image4.jpeg" />
         </div>
-      </div>
     </>
   );
 }
